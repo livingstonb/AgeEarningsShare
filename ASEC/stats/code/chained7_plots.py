06a57d2f2a0c1cd3afa21b0009862870579c6269
Brian Livingston <ak.brian09@gmail.com>
2018-08-22 11:48:35 -0500

    Added pop shares decomposition plots

--- ASEC/stats/code/chained7_plots.py
+++ ASEC/stats/code/chained7_plots.py
@@ -8,8 +8,11 @@ datadir = basedir + '/plot_data'
 plotdir = basedir + '/pyplots'
 if not os.path.exists(plotdir):
 	os.makedirs(plotdir)
-
+	
 genders = ['men','women']
+
+########################################################################
+# Unadjusted shares plots
 dashlist = [(1,5),(3,3,1,3),(3,5),(8,4,1,4),(8,4),(1,0)]
 for gender in genders:
 	filepath = datadir + '/unadjusted_' + gender + '.csv'
@@ -31,4 +34,38 @@ for gender in genders:
 
 	plotpath = plotdir + '/unadjusted_' + gender +'.png'
 	plt.savefig(plotpath)
+	
+########################################################################
+# Population shares decomposition
+dashlist = [(1,5),(3,5),(1,0)]
+
+for gender in genders:
+	filepath = datadir + '/agedecomp_' + gender + '.csv'
+	df = pd.read_csv(filepath,header=0,index_col=['agecat','year'])
+	df = df.reset_index()
+	for agegrp in [25,55]:
+		if agegrp == 25:
+			df_age = df[df['agecat'] == '25-34 year olds']
+		elif agegrp == 55:
+			df_age = df[df['agecat'] == '55-64 year olds']
+
+		fig, ax = plt.subplots(figsize=(5,5.5))
+		
+		components = ['age_effect','earnings_effect','zeroed_uearnshare']
+		labels = ['Population Share Component','Mean Earnings Component','Unadjusted Earnings Share']
+		for j,var in enumerate(components):
+			ax.plot(df_age['year'],df_age[var],label=labels[j],dashes=dashlist[j])
+
+		ax.set_xlim(1976,2017)
+		ax.set_xticks(np.arange(1976,2017,5))
+		#ax.set_ylim(0,0.5)
+		ax.set_xlabel('Year')
+		ax.legend(bbox_to_anchor=(0.8,-0.1),ncol=1,handlelength=3)
+	
+		plt.subplots_adjust(bottom=0.23,top=0.95,right=0.95)
+
+		plotpath = plotdir + '/agedecomp_' + gender + str(agegrp) +'.png'
+		plt.savefig(plotpath)
+			
+	
 plt.show()
