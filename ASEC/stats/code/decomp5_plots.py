a2405b1d86f8004e6144a8893917a5f402103d27
Brian Livingston <ak.brian09@gmail.com>
2018-09-05 10:37:24 -0500

    Updated python script to plot interaction terms

--- ASEC/stats/code/decomp5_plots.py
+++ ASEC/stats/code/decomp5_plots.py
@@ -6,7 +6,7 @@ import sys
 
 ## Plots decomposition data from output/plot_data
 
-basedir = '/Users/Brian/Documents/GitHub/AgeEarningsShare/ASEC/stats/output'
+basedir = '/Users/brianlivingston/Documents/GitHub/AgeEarningsShare/ASEC/stats/output'
 datadir = basedir + '/plot_data'
 plotdir = basedir + '/pyplots'
 if not os.path.exists(plotdir):
@@ -14,7 +14,6 @@ if not os.path.exists(plotdir):
 	
 genders = ['men','women']
 
-prefix = '/OB'
 
 ########################################################################
 # Unadjusted shares plots
@@ -40,25 +39,25 @@ for count,gender in enumerate(genders):
 	ax.set_title(titles[count])
 	
 ax.legend(bbox_to_anchor=(0.48,-0.13),ncol=2,handlelength=3)	
-plt.subplots_adjust(bottom=0.26,top=0.95,right=0.95,wspace=0.3)
+plt.subplots_adjust(bottom=0.26,top=0.95,right=0.97,wspace=0.3,left=0.05)
 
 plotpath = plotdir + '/unadjusted.png'
 plt.savefig(plotpath)
 
 ########################################################################
 # Population shares decomposition
-dashlist = [(1,5),(3,5),(1,0)]
+dashlist = [(1,5),(3,3,1,3),(3,5),(1,0)]
 subplotlist = [221,223,222,224]
-lcolorlist = ['b','g','r']
+lcolorlist = ['b','y','g','r']
 fig = plt.figure(figsize=(8,6.5))
 
-components = ['age_effect','earnings_effect','zeroed_uearnshare']
-labels = ['Population Share Component','Mean Earnings Component','Unadjusted Earnings Share']
+components = ['age_effect','earnings_effect','interact_effect','zeroed_uearnshare']
+labels = ['Population Share Component','Mean Earnings Component','Interaction','Unadjusted Earnings Share']
 titles = ['Men 25-34','Men 55-64','Women 25-34','Women 55-64']
 	
 count = 0
 for gender in genders:
-	filepath = datadir + prefix + '_agedecomp_' + gender + '.csv'
+	filepath = datadir + '/age_' + gender + '.csv'
 	df = pd.read_csv(filepath,header=0,index_col=['agecat','year'])
 	df = df.reset_index()
 	for agegrp in [25,55]:
@@ -80,23 +79,24 @@ for gender in genders:
 		
 ax.legend(bbox_to_anchor=(0.34,-0.22),ncol=1,handlelength=3)
 
-plt.subplots_adjust(bottom=0.22,top=0.95,left=0.09,right=0.95,hspace=0.4)
+plt.subplots_adjust(bottom=0.22,top=0.95,left=0.09,right=0.97,hspace=0.4)
 
-plotpath = plotdir + prefix + '_agedecomp.png'
+plotpath = plotdir + '/agedecomp.png'
 plt.savefig(plotpath)
 ########################################################################
 # Education decomposition
-dashlist = [(1,5),(3,3,1,3),(3,5),(1,0)]
+dashlist = [(1,5),(3,3,1,3),(3,5),(8,4,1,4),(1,0)]
 subplotlist = [221,223,222,224]
+lcolorlist = ['b','y','k','g','r']
 fig = plt.figure(figsize=(8,6.5))
 
-components = ['age_effect','college_effect','earnings_effect','zeroed_uearnshare']
-labels = ['Population Share Component','Education Component','Mean Earnings Component','Unadjusted Earnings Share']
+components = ['age_effect','college_effect','earnings_effect','interact_effect','zeroed_uearnshare']
+labels = ['Population Share Component','Education Component','Mean Earnings Component','Interaction','Unadjusted Earnings Share']
 titles = ['Men 25-34','Men 55-64','Women 25-34','Women 55-64']
 
 count = 0
 for gender in genders:
-	filepath = datadir + prefix + '_college_' + gender + '.csv'
+	filepath = datadir + '/college_' + gender + '.csv'
 	df = pd.read_csv(filepath,header=0,index_col=['agecat','year'])
 	df = df.reset_index()
 	for agegrp in [25,55]:
@@ -109,7 +109,7 @@ for gender in genders:
 		
 
 		for j,var in enumerate(components):
-			ax.plot(df_age['year'],df_age[var],label=labels[j],dashes=dashlist[j])
+			ax.plot(df_age['year'],df_age[var],label=labels[j],dashes=dashlist[j],color=lcolorlist[j])
 
 		ax.set_xlim(1976,2017)
 		ax.set_xticks(np.arange(1976,2017,10))
@@ -119,24 +119,25 @@ for gender in genders:
 
 ax.legend(bbox_to_anchor=(0.34,-0.22),ncol=1,handlelength=3)
 
-plt.subplots_adjust(bottom=0.22,top=0.95,left=0.09,right=0.95,hspace=0.4)
+plt.subplots_adjust(bottom=0.25,top=0.95,left=0.09,right=0.97,hspace=0.4)
 
-plotpath = plotdir + prefix +'_college.png'
+plotpath = plotdir + '/college.png'
 plt.savefig(plotpath)
 		
 ########################################################################
 # Hours decomposition
-dashlist = [(1,5),(3,3,1,3),(3,5),(1,0)]
+dashlist = [(1,5),(3,3,1,3),(3,5),(8,4,1,4),(1,0)]
 subplotlist = [221,223,222,224]
+lcolorlist = ['b','y','k','g','r']
 fig = plt.figure(figsize=(8,6.5))
 
-components = ['age_effect','hours_effect','earnings_effect','zeroed_uearnshare']
-labels = ['Population Share Component','Hours Component','Mean Earnings Component','Unadjusted Earnings Share']
+components = ['age_effect','hours_effect','earnings_effect','interact_effect','zeroed_uearnshare']
+labels = ['Population Share Component','Hours Component','Mean Earnings Component','Interaction','Unadjusted Earnings Share']
 titles = ['Men 25-34','Men 55-64','Women 25-34','Women 55-64']
 
 count = 0
 for gender in genders:
-	filepath = datadir + prefix + '_hours_' + gender + '.csv'
+	filepath = datadir + '/hours_' + gender + '.csv'
 	df = pd.read_csv(filepath,header=0,index_col=['agecat','year'])
 	df = df.reset_index() 
 	for agegrp in [25,55]:
@@ -149,7 +150,7 @@ for gender in genders:
 		
 
 		for j,var in enumerate(components):
-			ax.plot(df_age['year'],df_age[var],label=labels[j],dashes=dashlist[j])
+			ax.plot(df_age['year'],df_age[var],label=labels[j],dashes=dashlist[j],color=lcolorlist[j])
 
 		ax.set_xlim(1976,2017)
 		ax.set_xticks(np.arange(1976,2017,10))
@@ -159,24 +160,25 @@ for gender in genders:
 
 ax.legend(bbox_to_anchor=(0.34,-0.22),ncol=1,handlelength=3)
 
-plt.subplots_adjust(bottom=0.22,top=0.95,left=0.09,right=0.95,hspace=0.4)
+plt.subplots_adjust(bottom=0.25,top=0.95,left=0.09,right=0.97,hspace=0.4)
 
-plotpath = plotdir + prefix +'_hours.png'
+plotpath = plotdir + '/hours.png'
 plt.savefig(plotpath)
 		
 ########################################################################
 # EMS (educ/married/services) decomposition
-dashlist = [(1,5),(3,3,1,3),(3,5),(1,0)]
+dashlist = [(1,5),(3,3,1,3),(3,5),(8,4,1,4),(1,0)]
 subplotlist = [221,223,222,224]
+lcolorlist = ['b','y','k','g','r']
 fig = plt.figure(figsize=(8,6.5))
 
-components = ['age_effect','ems_effect','earnings_effect','zeroed_uearnshare']
-labels = ['Population Share Component','Educ/Married/Services Component','Mean Earnings Component','Unadjusted Earnings Share']
+components = ['age_effect','ems_effect','earnings_effect','interact_effect','zeroed_uearnshare']
+labels = ['Population Share Component','Educ/Married/Services Component','Mean Earnings Component','Interaction','Unadjusted Earnings Share']
 titles = ['Men 25-34','Men 55-64','Women 25-34','Women 55-64']
 
 count = 0
 for gender in genders:
-	filepath = datadir + prefix + '_ems_' + gender + '.csv'
+	filepath = datadir + '/ems_' + gender + '.csv'
 	df = pd.read_csv(filepath,header=0,index_col=['agecat','year'])
 	df = df.reset_index()
 	for agegrp in [25,55]:
@@ -188,20 +190,19 @@ for gender in genders:
 		ax = fig.add_subplot(subplotlist[count])
 		
 		for j,var in enumerate(components):
-			ax.plot(df_age['year'],df_age[var],label=labels[j],dashes=dashlist[j])
+			ax.plot(df_age['year'],df_age[var],label=labels[j],dashes=dashlist[j],color=lcolorlist[j])
 
 		ax.set_xlim(1976,2017)
 		ax.set_xticks(np.arange(1976,2017,10))
 		ax.set_title(titles[count])
 		# ax.set_xlabel('Year')
 		count += 1
-	
-		plt.subplots_adjust(bottom=0.28,top=0.95,right=0.95)
+
 ax.legend(bbox_to_anchor=(0.34,-0.22),ncol=1,handlelength=3)
 
-plt.subplots_adjust(bottom=0.22,top=0.95,left=0.09,right=0.95,hspace=0.4)
+plt.subplots_adjust(bottom=0.25,top=0.95,left=0.09,right=0.97,hspace=0.4)
 
-plotpath = plotdir + prefix +'_ems.png'
+plotpath = plotdir + '/ems.png'
 plt.savefig(plotpath)
 	
 plt.show()
